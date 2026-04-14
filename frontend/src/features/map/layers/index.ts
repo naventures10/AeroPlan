7336e6caf6f9f8172a375ed3c7bbbe0e69fc2e54
naveen <naveendevapalan10@icloud.com>
2026-04-14 12:40:16 +0530

    refactor: decompose monolithic useDeckLayers hook into modular layer factories

---
+++ frontend/src/features/map/layers/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Barrel export for the layers module.
+ *
+ * Consumers import from `./layers` and get the same public API.
+ */
+export { useDeckLayers } from './useDeckLayers';
+export { POLYGON_PAINT, POINT_PAINT } from './mapStyles';
